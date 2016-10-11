767736a5ac948c089e3fb9a96595e1eead8da0b6
miamiasheep <sheeperic@gmail.com>
2016-10-11 15:43:58 +0800

    change to leetcode solution format

--- leetcode/word_pattern/wordPattern.py
+++ leetcode/word_pattern/wordPattern.py
@@ -1,31 +1,35 @@
-def wordPattern(pattern, str):
-    strings = str.split(' ')
-    exist = set()
-    save = {}
-    
-    if len(pattern) != len(strings):
-        return False
-    
-    for i, v in enumerate(pattern):
-        if v not in save:
-            if strings[i] in exist:
-                return False
-            save[v] = strings[i]
-        else:
-            if save[v] != strings[i]:
-                return False
-        exist.add(strings[i])
-    return True
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        strings = str.split(' ')
+        exist = set()
+        save = {}
+
+        if len(pattern) != len(strings):
+            return False
+
+        for i, v in enumerate(pattern):
+            if v not in save:
+                if strings[i] in exist:
+                    return False
+                save[v] = strings[i]
+            else:
+                if save[v] != strings[i]:
+                    return False
+            exist.add(strings[i])
+        return True
 
 if __name__ == '__main__':
+    s = Solution()
     pattern = 'abba'
     str = 'dog dog dog dog'
-    print wordPattern(pattern, str)
+    assert s.wordPattern(pattern, str) == False
     
     pattern = 'abba'
     str = 'dog cat cat dog'
-    print wordPattern(pattern, str)
+    assert s.wordPattern(pattern, str) == True
     
     pattern = 'abba'
     str = 'dog cat cat fish'
-    print wordPattern(pattern, str)
+    assert s.wordPattern(pattern, str) == False
+    
+    print 'complete!'
